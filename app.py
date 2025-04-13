f50cb944d96ad37ef6c6cf1f5ccad35d3067702f
Andric Ishami <ishamand7@gmail.com>
2025-04-13 22:48:22 +0300

    update the shareable parameter

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ iface = gr.Interface(
 )
 
 # Modified launch configuration
+print("About to lauch app...")
 iface.launch(server_port=7860, server_name="0.0.0.0", share=True)
-#this technology is a step towards smart farming
